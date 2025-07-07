c7cefd60378e5a97fdff12b4617f123567c616dd
준용 <wnsdyd1513@gmail.com>
2025-07-07 14:41:49 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+import Menubar from './components/common/Menubar';
+import Footer from './components/common/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* 라우터 등록은 App.js 에서 설정함 */}
+      <Menubar />
+      메인페이지
+      <Footer />
+    </Router>
   );
 }
 
