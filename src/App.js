33e7fd38a4a4899f065fc7ea85bb6fd2dd0371d0
준용 <wnsdyd1513@gmail.com>
2025-07-08 09:19:18 +0900

    메뉴바수정

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import AppRouter from './routers/router';
 import Menubar from './components/common/Menubar';
 import Footer from './components/common/Footer';
 
 function App() {
   return (
     <Router>
-      {/* 라우터 등록은 App.js 에서 설정함 */}
       <Menubar />
-      메인페이지
+      <AppRouter />
       <Footer />
     </Router>
   );
