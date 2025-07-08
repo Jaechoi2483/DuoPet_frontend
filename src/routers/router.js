19ce2f7a020c8df39c34379b4da95b5c4916d058
Starchatcher <snopia@naver.com>
2025-07-09 08:46:49 +0900

    인사말페이지수정

--- src/routers/router.js
+++ src/routers/router.js
@@ -2,34 +2,27 @@
 
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-<<<<<<< HEAD
+
+import HomePage from '../pages/HomePage';
 import DuoPetGreeting from '../components/common/DuoPetGreeting';
-
-const AppRouter = () => {
-  return <Routes>
-  <Route path="/greeting" element={<DuoPetGreeting />} />
-  </Routes>;
-=======
 import noticeRoutes from './noticeRoutes';
-
 import userRoutes from './userRoutes';
 import freeBoardRoutes from './freeBoardRoutes';
-
 import healthRoutes from './healthRoutes';
 import adminRoutes from './adminRoutes';
 
 const AppRouter = () => {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/greeting" element={<DuoPetGreeting />} />
       {userRoutes}
-      {noticeRoutes}
       {noticeRoutes}
       {freeBoardRoutes}
       {healthRoutes}
       {adminRoutes}
     </Routes>
   );
->>>>>>> bba0ac55e35799ffcfd9a6eb684a926f518bd4c0
 };
 
 export default AppRouter;
