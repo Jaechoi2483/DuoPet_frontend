dbdd27b88e6ebac840736ab51b4e68cec422a4af
Starchatcher <snopia@naver.com>
2025-07-09 20:17:52 +0900

    오류수정

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@
 import React from 'react';
 import { BrowserRouter as Router, useLocation } from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
 import AppRouter from './routers/router';
 import Menubar from './components/common/Menubar';
 import Footer from './components/common/Footer';
 
 import DuoPetGreeting from './components/common/DuoPetGreeting';
 import { SignupProvider } from './components/context/SignupContext';
-
 
 // 상세페이지에서만 Footer 제거용 래퍼 컴포넌트
 function AppWrapper() {
