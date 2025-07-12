49767b13149afac2a636e8818b5167b1b9b4f4d7
준용 <wnsdyd1513@gmail.com>
2025-07-12 15:26:48 +0900

    관리자페이지통계 표시

--- src/pages/admin/Qna.js
+++ src/pages/admin/Qna.js
@@ -122,6 +122,9 @@ function Qna() {
           ))}
         </div>
         <button className={styles.writeButton} onClick={handleWriteClick}>
+          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
           글쓰기
         </button>
       </div>
--- src/pages/notice/NoticeList.js
+++ src/pages/notice/NoticeList.js
@@ -101,6 +101,9 @@ function NoticeList() {
           className={styles.writeButton}
           onClick={() => navigate('/notice/write')}
         >
+          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
           글쓰기
         </button>
       </div>
